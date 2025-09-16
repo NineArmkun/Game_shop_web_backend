2531ce41904ea739811fd703379c7e4a44bc1431
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-17 00:22:35 +0700

    cors

--- app.ts
+++ app.ts
@@ -1,23 +1,27 @@
 import express from "express";
 import morgan from "morgan";
-import cor from "cors";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 import { router as index } from "./controller/index";
 import { router as user } from "./controller/user";
 import { router as login } from "./controller/login";
 import { router as lotto } from "./controller/lotto";
+
 export const app = express();
 
+// เรียกใช้ cors ก่อน route อื่น ๆ
+app.use(cors({
+    origin: "*",  // หรือ "*" เพื่ออนุญาตทุกโดเมน (สำหรับ dev เท่านั้น)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-// app.use(morgan('dev'));
-// app.use(    cor()  );
-// app.use(bodyParser.text);
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
 app.use("/user", user);
 app.use("/", index);
 app.use("/login", login);
-app.use("/lotto", lotto)
-// app.use("/", (req, res) => {
-//   res.send("Hello World!!!");
-// });
+app.use("/lotto", lotto);
--- dist/app.js
+++ dist/app.js
@@ -5,20 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const index_1 = require("./controller/index");
 const user_1 = require("./controller/user");
 const login_1 = require("./controller/login");
 const lotto_1 = require("./controller/lotto");
 exports.app = (0, express_1.default)();
-// app.use(morgan('dev'));
-// app.use(    cor()  );
-// app.use(bodyParser.text);
+// เรียกใช้ cors ก่อน route อื่น ๆ
+exports.app.use((0, cors_1.default)({
+    origin: "*", // หรือ "*" เพื่ออนุญาตทุกโดเมน (สำหรับ dev เท่านั้น)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 exports.app.use(body_parser_1.default.json());
+exports.app.use((0, morgan_1.default)('dev'));
 exports.app.use("/user", user_1.router);
 exports.app.use("/", index_1.router);
 exports.app.use("/login", login_1.router);
 exports.app.use("/lotto", lotto_1.router);
-// app.use("/", (req, res) => {
-//   res.send("Hello World!!!");
-// });
