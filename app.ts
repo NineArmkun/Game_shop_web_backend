5bb8cca31a8c2c681307b6c62068e1517a299674
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-17 01:38:28 +0700

    ..

--- app.ts
+++ app.ts
@@ -2,10 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import cor from "cors";
 import bodyParser from "body-parser";
-
+import { router as login } from "./controller/login";
 import { router as index } from "./controller/index";
 import { router as user } from "./controller/user";
-import { router as login } from "./controller/login";
 import { router as lotto } from "./controller/lotto";
 export const app = express();
 
