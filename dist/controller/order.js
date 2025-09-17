6251c376c4a5f5ee27cbb216509461d56d9b7c8c
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-17 19:04:23 +0700

    orders

--- dist/controller/order.js
+++ dist/controller/order.js
@@ -17,7 +17,7 @@ exports.router.get("/:id", async (req, res) => {
     try {
         const connect = await DBconnect_1.conn;
         let id = +req.params.id;
-        const [rows] = await connect.query("select * from orders where uid = ?", [id]);
+        const [rows] = await connect.query("select * from orders ,lotto where lotto.lid = orders.lid and orders.lid = ? ", [id]);
         res.send(rows);
     }
     catch (err) {
