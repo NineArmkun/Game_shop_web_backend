bb55dd7d24f243c67855c7e4bdb27e250c5baa51
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-19 14:49:21 +0700

    dist

--- dist/controller/order.js
+++ dist/controller/order.js
@@ -12,7 +12,7 @@ exports.router.get("/", async (req, res) => {
     const [rows] = await DBconnect_1.conn.query("select * from orders");
     res.send(rows);
 });
-//select ilid by lotto 
+//select ilid by lotto
 exports.router.get("/:id/paid", async (req, res) => {
     try {
         const connect = await DBconnect_1.conn;
@@ -47,25 +47,34 @@ exports.router.get("/:id/pending", async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
-exports.router.post('/pay', async (req, res) => {
+//insert orders
+exports.router.post("/order", (req, res) => {
+    try {
+    }
+    catch (error) { }
+});
+exports.router.post("/pay", async (req, res) => {
     const { uid, oid, price } = req.body;
     try {
         const [users] = await DBconnect_1.conn.query("SELECT money FROM user WHERE uid = ?", [uid]);
         if (!users || users.length === 0) {
-            return res.status(404).json({ message: 'ไม่พบผู้ใช้' });
+            return res.status(404).json({ message: "ไม่พบผู้ใช้" });
         }
         const user = users[0];
         if (user.money < price) {
-            return res.status(400).json({ message: 'ยอดเงินไม่เพียงพอ' });
+            return res.status(400).json({ message: "ยอดเงินไม่เพียงพอ" });
         }
         // หักเงิน
-        await DBconnect_1.conn.query("UPDATE user SET money = money - ? WHERE uid = ?", [price, uid]);
+        await DBconnect_1.conn.query("UPDATE user SET money = money - ? WHERE uid = ?", [
+            price,
+            uid,
+        ]);
         // อัปเดตสถานะการชำระเงิน
         await DBconnect_1.conn.query("UPDATE orders SET payment_status = 'paid' WHERE oid = ?", [oid]);
-        res.status(200).json({ message: 'ชำระเงินสำเร็จ' });
+        res.status(200).json({ message: "ชำระเงินสำเร็จ" });
     }
     catch (error) {
-        console.error('เกิดข้อผิดพลาด:', error);
-        res.status(500).json({ message: 'เกิดข้อผิดพลาด' });
+        console.error("เกิดข้อผิดพลาด:", error);
+        res.status(500).json({ message: "เกิดข้อผิดพลาด" });
     }
 });
