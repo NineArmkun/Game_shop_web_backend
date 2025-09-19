a5f7532a7af4296a1780621646ef8ee981e7b4ad
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-19 16:01:30 +0700

    111

--- controller/order.ts
+++ controller/order.ts
@@ -108,7 +108,8 @@ router.post("/check_lotto", async (req, res) => {
         log(check_lotto);
 
     } catch (err) {
-
+        console.error("Error adding lotto entry:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
 })
 
