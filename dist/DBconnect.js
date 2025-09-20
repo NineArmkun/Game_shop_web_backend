7633b0321f3ac1c9f5a35331d4d0249d2863b5c4
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-20 22:06:46 +0700

    00

--- dist/DBconnect.js
+++ dist/DBconnect.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.conn = void 0;
 const promise_1 = __importDefault(require("mysql2/promise"));
 exports.conn = promise_1.default.createPool({
-    connectionLimit: 20,
+    connectionLimit: 10,
     host: "202.28.34.203",
     user: "mb68_66011212013",
     password: "xjY_1gE3I(!Y",
